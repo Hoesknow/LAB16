6fda3df8dd199564f37e5e055303d9ba207acd4f
Richard <Lesko50@gmail.com>
2017-03-15 19:13:45 -0400

    modulechanges

--- module.js
+++ module.js
@@ -9,7 +9,8 @@ app.controller('talksCtrl', function($scope){
 ,'Tell me how you really feel, tell me how you really feel'
 ,'Top floor the view alone will leave you breathless *gasps*  Try to catch it, its kinda hard Getting choked by detectives, yeah, yeah, now check the method'
 ,'You wore a pink diamond chicken wing chain (Are you dumb?)'
-,'Take a little trip, hater pack up yo mind Look forward not behind, then youll see what you find","The story of life is quicker than the wink of an eye, the story of love is hello and goodbye. until we meet again'
+,'Take a little trip, hater pack up yo mind Look forward not behind, then youll see what you find'
+,'The story of life is quicker than the wink of an eye, the story of love is hello and goodbye. until we meet again'
 ,'website website website website.... droptop'
  ,'who let the dogs out?'];
 var moreDeco  =['style6', 'style7', 'style8', 'style9', 'style10'];
@@ -27,6 +28,7 @@ var moreDeco  =['style6', 'style7', 'style8', 'style9', 'style10'];
      item.word=moreWords[Math.floor(Math.random() * moreWords.length)];
      $scope.wordList.push(item);
      };
+
      $scope.addDecorativeWord=function() {
        var item={};
        item.word=words[Math.floor(Math.random() * words.length)];
